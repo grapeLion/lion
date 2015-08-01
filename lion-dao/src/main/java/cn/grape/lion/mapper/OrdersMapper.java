79d0cd89a65312b970bcdca8e1f8f42106f190c0
solis <wz_gg_2008@126.com>
2015-08-01 14:41:59 +0800

    更新

--- lion-dao/src/main/java/cn/grape/lion/mapper/OrdersMapper.java
+++ lion-dao/src/main/java/cn/grape/lion/mapper/OrdersMapper.java
@@ -1,28 +1,15 @@
 package cn.grape.lion.mapper;
 
 import cn.grape.lion.domain.Orders;
-import cn.grape.lion.domain.OrdersExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface OrdersMapper {
-    int countByExample(OrdersExample example);
-
-    int deleteByExample(OrdersExample example);
-
     int deleteByPrimaryKey(Integer id);
 
     int insert(Orders record);
 
     int insertSelective(Orders record);
 
-    List<Orders> selectByExample(OrdersExample example);
-
     Orders selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
-
-    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
 
     int updateByPrimaryKeySelective(Orders record);
 
